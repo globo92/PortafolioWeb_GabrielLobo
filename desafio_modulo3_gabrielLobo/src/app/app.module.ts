6af8433ba83b4a404bf64bf8ae91559bea3bb6c2
globo92 <gabrielelobop@gmail.com>
2022-10-18 21:08:46 -0300

    agregando el gooter

--- desafio_modulo3_gabrielLobo/src/app/app.module.ts
+++ desafio_modulo3_gabrielLobo/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { ProyectosComponent } from './components/proyectos/proyectos.component';
     ExperienciaComponent,
     EducacionComponent,
     HysComponent,
-    ProyectosComponent
+    ProyectosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
